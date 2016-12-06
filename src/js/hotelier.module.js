b2d768e85eb03ab9635951abee9fdd77db811f80
J Ridgeway <sw333zy@gmail.com>
2016-12-06 15:06:57 -0500

    made guest views but need grunt

--- src/js/hotelier.module.js
+++ src/js/hotelier.module.js
@@ -7,6 +7,20 @@
   routerConfig.$inject=['$stateProvider'];
 
   function routerConfig($stateProvider) {
+    $stateProvider
+      .state({
+        name: 'home',
+        url: '',
+        templateUrl: 'views/guest.template.html'
+
+      })
+
+      .state({
+        name: 'rooms-available',
+        url: '/rooms',
+        templateUrl: 'views/rooms-avalable.template.html'
+
+      })
 
   }
 }());
