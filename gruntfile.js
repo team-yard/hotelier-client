37d72588af029e9dfca1772266f689579432b844
Jessica Dembe <jmdembe@gmail.com>
2016-12-06 17:11:27 -0500

    Added needed grunt processes for project

--- gruntfile.js
+++ gruntfile.js
@@ -1,61 +1,66 @@
 'use strict';
 
 module.exports = function(grunt) {
-  'use strict'
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        clean: [ 'build/' ],
+    clean: [ 'build/' ],
 
-        jshint: {
-            options: {
-                jshintrc: '.jhintrc',
-                ignores: [ 'node_modules/**' ]
-            },
-            source: {
-              files: {
-                src: ['src/js/**/*.js']
-              }
-            },
-        },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        ignores: [ 'node_modules/**' ]
+      },
+      source: {
+        files: {
+          src: ['src/js/**/*.js']
+        }
+      },
+    },
 
-        copy: {
-            html: {
-                files: [
-                    {
-                      expand: true,
-                      cwd: 'src/',
-                      src: 'index.html',
-                      dest: 'build/'
-                    }
-                ]
-            }
-            vendorjs: {
-              files: [
-                {
-                  expand: true,
-                  cwd: 'node_modules/angular',
-                  src: [ 'angular.js' ],
-                  dest: 'build/js/'
-                },
-                {
-                  expand: true,
-                  cwd: 'node_modules/angular-ui-router',
-                  src: [ 'angular-ui-router.js' ],
-                  dest: 'build/js/'
-                }
-              ]
-            },
-        },
+    copy: {
+      html: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: 'index.html',
+            dest: 'build/'
+          }
+        ]
+      },
+      vendorjs: {
+        files: [
+          {
+            expand: true,
+            cwd: 'node_modules/angular',
+            src: [ 'angular.js' ],
+            dest: 'build/js/'
+          },
+          {
+            expand: true,
+            cwd: 'node_modules/angular-ui-router',
+            src: [ 'angular-ui-router.js' ],
+            dest: 'build/js/'
+          }
+        ]
+      },
+    },
 
-    });
+    concat: {
+      js: {
+        src: [ 'src/js/**/*.js' ],
+        dest: 'build/js/app.js'
+      }
+    }
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('')
+  });
 
-    grunt.registerTask('default', [ 'copy' ]);
-    grunt.registerTask('build', )
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', [ 'clean', 'jshint', 'copy', 'concat' ]);
 };
