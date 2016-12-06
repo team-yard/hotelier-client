2cf9b9e31c128d3d8fa972d07db7242435f03300
Jessica Dembe <jmdembe@gmail.com>
2016-12-06 16:46:05 -0500

    Installed grunt packages to clean, copy, concat, jshint

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,61 @@
 'use strict';
 
 module.exports = function(grunt) {
+  'use strict'
 
     grunt.initConfig({
+
+        clean: [ 'build/' ],
+
+        jshint: {
+            options: {
+                jshintrc: '.jhintrc',
+                ignores: [ 'node_modules/**' ]
+            },
+            source: {
+              files: {
+                src: ['src/js/**/*.js']
+              }
+            },
+        },
 
         copy: {
             html: {
                 files: [
-                    { expand: true, cwd: 'src/', src: 'index.html', dest: 'build/' }
+                    {
+                      expand: true,
+                      cwd: 'src/',
+                      src: 'index.html',
+                      dest: 'build/'
+                    }
                 ]
             }
-        }
+            vendorjs: {
+              files: [
+                {
+                  expand: true,
+                  cwd: 'node_modules/angular',
+                  src: [ 'angular.js' ],
+                  dest: 'build/js/'
+                },
+                {
+                  expand: true,
+                  cwd: 'node_modules/angular-ui-router',
+                  src: [ 'angular-ui-router.js' ],
+                  dest: 'build/js/'
+                }
+              ]
+            },
+        },
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('')
 
     grunt.registerTask('default', [ 'copy' ]);
+    grunt.registerTask('build', )
 };
