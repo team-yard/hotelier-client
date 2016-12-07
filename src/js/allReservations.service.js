68b8bd7682d9aba2e090759f67165577c7bfce7c
Syed Ali <sali3@cox.net>
2016-12-07 04:52:13 -0500

    Refactoring code in service and controller for all reservations

---
+++ src/js/allReservations.service.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+
+  angular.module('hotlier')
+    .factory('AllReservationsService', AllReservationsService);
+
+  AllRerservationsService.$inject = ['$http'];
+
+  function AllRerservationsService($http){
+    console.log('creating AllRerservationsService');
+
+    return {
+      getReservations: getReservations
+    };
+
+
+    /**
+    * search api for list of all reservations
+    * @return {Promise} the completed ajax call promise
+    */
+    function getReservations(){
+      return $http({
+        url:'https://hotelier-api-iron.herokuapp.com/api/',
+        method: 'GET /Reservations'
+      })
+      .then(function onlyReturnData(response){
+
+        return response.data;
+
+      });
+
+
+    }
+
+
+  }
+
+
+}());
