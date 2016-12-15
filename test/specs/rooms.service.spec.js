8acd4ef54fa7dc0e3110e934b3de88c3b9c50e70
J Ridgeway <sw333zy@gmail.com>
2016-12-15 08:10:34 -0500

    passing tests

--- test/specs/rooms.service.spec.js
+++ test/specs/rooms.service.spec.js
@@ -10,90 +10,59 @@
 
         beforeEach(module('hotelier'));
 
+        console.log('hello');
+
         beforeEach(inject(function(_$rootScope_, _$httpBackend_, _RoomsService_) {
             RoomsService = _RoomsService_;
             $httpBackend = _$httpBackend_;
             $rootScope = _$rootScope_;
+            console.log('hello');
 
             $httpBackend
                 .whenGET('https://hotelier-api-yard.herokuapp.com/api/Rooms')
-                .respond({
+                .respond([{
                     id: '5842d47de8bf1b1100e4665e',
                     maxOccupancy: 8,
                     number: 301,
                     rate: 400
-                });
-
+                }]);
+                console.log('hello');
             // mock out the request for the home template
             $httpBackend
-                .whenGET('views/rooms-available.template.html')
+                .whenGET('views/guest.template.html')
                 .respond('whatever you want to put here');
         }));
 
-        it('should be able to get repo details given good data', function(doneCallback) {
+        it('should be able to get rooms', function(doneCallback) {
 
             var result = RoomsService.getRooms();
-            expect(response).to.be.a('object');
-            expect(response.then).to.be.a('function');
-            // expect(result.catch).to.be.a('function');
+            expect(result).to.be.a('object');
+            expect(result.then).to.be.a('function');
+            expect(result.catch).to.be.a('function');
+
 
             // result
-            //     .then(function(data) {
-            //         expect(data.name).to.equal('jquery-mockjax');
-            //         doneCallback();
-            //     })
-            //     .catch(function() {
-            //         doneCallback('Should not reject this promise');
-            //     });
-        //
-        //     $httpBackend.flush(); // go ahead and send the (fake) response now
-        // });
-        //
-        // it('should fail if no username or reponame provided', function(doneCallback) {
-        //     var result = RepoService.getRepo();
-        //
-        //     result
-        //         .then(function() {
-        //             doneCallback('Should not resolve if no username');
-        //         })
-        //         .catch(function(err) {
-        //             expect(err).to.be.an.instanceof(Error);
-        //             doneCallback();
-        //         });
-        //
-        //     $rootScope.$digest();
-        // });
+            //   .then
+                //inside then and catch
+                result
+                  .then(function(data){
+                    console.log('what am i?', data);
+                    console.log(data.maxOccupancy);
+                    expect(data).to.be.an('array');
+                    expect(result.maxOccupancy).to.equal(undefined);
+                    expect(data[0]).to.include.keys('id', 'maxOccupancy', 'number', 'rate');
+
+
+
+                    doneCallback();
+                  })
+                  .catch(function(){
+                    doneCallback('Should not reject this promise');
+                  });
+
+            $httpBackend.flush();
+        });
 
     });
 
 })();
-
-
-// (function() {
-//   'use strict';
-//
-//   var expect = chai.expect;
-//
-//   describe('RoomsService', function() {
-//       var RoomsService;
-//
-//
-//       beforeEach(module('hotelier'));
-//
-//
-//       beforeEach(inject(function(_RoomsService_) {
-//
-//           RoomsService = _RoomsService_;
-//       }));
-//
-//       it('should be able to get an array of items', function() {
-//
-//           var result = RoomsService.getRooms();
-//           expect(result).to.be.an('object');
-// //           // expect(result.length).to.equal(19);
-// //
-//       });
-//
-//     });
-//
-}());
